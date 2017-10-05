9b95023a38178da1945265f7f3d154ed79392c58
Mike Trizna <triznam@si.edu>
2017-10-05 11:57:23 -0400

    Pulled in GenBank fetch from id list functions

--- genetic_collections/ncbi_functions.py
+++ genetic_collections/ncbi_functions.py
@@ -4,6 +4,7 @@ from lxml import objectify
 import json
 from collections import namedtuple
 from operator import itemgetter
+import re
 
 
 def ncbi_inst_search(search_term):
@@ -122,3 +123,91 @@ def gb_search(format='variable', **kwargs):
         print('Not a valid format')
 
     return
+
+def gb_fetch_from_id_list(id_list, batch_size=500):
+    """
+    Orchestrates making calls to the NCBI efetch service, and passes off the 
+    XML to the parse_fetch_results function. 
+    
+    Parameters
+    ----------
+
+    batch_size : int, the number of results to request at a time -- the higher
+                      the better, but too large of a result set causes errors
+    
+    Returns
+    -------
+    parsed_results : list of dicts, all results
+    """
+
+    result_count = len(id_list)
+    parsed_results = []
+    i = 0
+    while i < result_count:
+        list_for_url = ','.join(id_list[i : i+batch_size])
+        url_base = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+        params = {'db': 'nuccore',
+                  'rettype': 'gb',
+                  'retmode': 'xml',
+                  'id': list_for_url}
+        r = requests.get(url_base, params=params)
+        result_list = gb_parse_xml_fetch_results(r.content)
+        parsed_results += result_list
+
+        i += batch_size
+    return parsed_results
+
+def gb_parse_xml_fetch_results(gb_xml):
+    target_pieces = ['specimen_voucher','country','lat_lon','collection_date',
+                     'collected_by','identified_by','bio_material',
+                     'culture_collection','PCR_primers']
+    result_list = []
+    huge_parser = objectify.makeparser(huge_tree=True)
+    xml_results = objectify.fromstring(gb_xml, huge_parser)
+    for gb in xml_results.GBSeq:
+        result = {}
+        try:
+            result['accession'] = gb['GBSeq_primary-accession'].text
+            result['scientific_name'] = gb['GBSeq_organism'].text
+            result['publish_date'] = gb['GBSeq_create-date'].text
+            result['update_date'] = gb['GBSeq_update-date'].text
+            result['keyword'] = ''
+            if hasattr(gb, 'GBSeq_xrefs'):
+                for xref in gb['GBSeq_xrefs']['GBXref']:
+                    if xref['GBXref_dbname'].text == 'BioProject':
+                        result['bioproject'] = xref['GBXref_id'].text
+                    elif xref['GBXref_dbname'].text == 'Sequence Read Archive':
+                        result['sra'] = xref['GBXref_id'].text
+                    elif xref['GBXref_dbname'].text == 'BioSample':
+                        result['biosample'] = xref['GBXref_id'].text
+            if hasattr(gb, 'GBSeq_keywords'):
+                for keyword in gb['GBSeq_keywords'].iterchildren():
+                    if keyword.text == 'BARCODE':
+                        result['keyword'] = 'BARCODE'
+            result['seq_len'] = gb.GBSeq_length.text
+            for ref in gb['GBSeq_references']['GBReference']:
+                if hasattr(ref, 'GBReference_title'):
+                    if ref['GBReference_title'] == 'Direct Submission':
+                        if hasattr(ref, 'GBReference_authors'):
+                            result['submit_authors'] = '; '.join([author.text for author in ref['GBReference_authors']['GBAuthor']])
+                        submit_inst_text = ref['GBReference_journal'].text
+                        inst_split = re.split('[\(\)]',submit_inst_text)
+                        result['submit_date'] = inst_split[1]
+                        result['submit_inst'] = inst_split[2]
+            for feature in gb['GBSeq_feature-table'].iterchildren():
+                if feature.GBFeature_key.text == 'source':
+                    for feature_qual in feature.GBFeature_quals.iterchildren():
+                        if feature_qual.GBQualifier_name.text == 'db_xref':
+                            if 'taxon' in feature_qual.GBQualifier_value.text:
+                                result['taxid'] = str(feature_qual.GBQualifier_value.text.split(":")[1])
+                            elif 'BOLD' in feature_qual.GBQualifier_value.text:
+                                result['bold_id'] = str(feature_qual.GBQualifier_value.text.split(":")[1])
+                        elif feature_qual.GBQualifier_name.text in target_pieces:
+                            if feature_qual.GBQualifier_name.text in result:
+                                result[feature_qual.GBQualifier_name.text] += ';' + str(feature_qual.GBQualifier_value.text)
+                            else:
+                                result[feature_qual.GBQualifier_name.text] = str(feature_qual.GBQualifier_value.text)
+        except:
+            print(gb['GBSeq_primary-accession'].text)
+        result_list.append(result)
+    return result_list

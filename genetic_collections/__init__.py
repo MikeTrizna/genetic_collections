4371c52ce685f62f48e95c0d06aa4f39ca91dc52
Mike Trizna <triznam@si.edu>
2017-11-30 14:56:32 -0500

    Bump version: 0.1.6 → 0.1.7

--- genetic_collections/__init__.py
+++ genetic_collections/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Mike Trizna"""
 __email__ = 'mike.trizna@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 from .ncbi_functions import *
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='genetic_collections',
-    version='0.1.6',
+    version='0.1.7',
     description="A Python library for connecting genetic records with specimen data.",
     long_description=readme + '\n\n' + history,
     author="Mike Trizna",

2302042ea98a1c487fce4836f867cbe603752d1e
Amanda Devine <devine.amanda.m@gmail.com>
2018-01-23 16:00:17 -0500

    Changed default api_key value from False to None, and updated docstrings

--- genetic_collections/ncbi_functions.py
+++ genetic_collections/ncbi_functions.py
@@ -7,7 +7,7 @@ from operator import itemgetter
 import re
 
 
-def ncbi_inst_search(search_term, api_key = False):
+def ncbi_inst_search(search_term, api_key = None):
 
     search_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
     search_params = {'term': search_term,
@@ -67,7 +67,7 @@ def parse_biocollection_xml(xml):
         result_list.append(parsed_result)
     return result_list
 
-def gb_search(format='variable', api_key = False, **kwargs):
+def gb_search(format='variable', api_key = None, **kwargs):
 
     search_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
 
@@ -128,7 +128,7 @@ def gb_search(format='variable', api_key = False, **kwargs):
 
     return
 
-def gb_fetch_from_id_list(id_list, batch_size=500, api_key = False):
+def gb_fetch_from_id_list(id_list, batch_size=500, api_key = None):
     """
     Orchestrates making calls to the NCBI efetch service, and passes off the 
     XML to the parse_fetch_results function. 
@@ -138,6 +138,10 @@ def gb_fetch_from_id_list(id_list, batch_size=500, api_key = False):
     :param int batch_size:
         The number of results to request at a time -- the higher the better, but 
         too large of a result set causes errors
+    :param str api_key:
+        A personal NCBI API key, obtained by creating an NCBI account. An API key 
+        is not required, but supplying one will increase API call rate to NCBI from 
+        three per second to 10 per second. 
     :rtype: list of dictionaries
     """
 
@@ -217,7 +221,7 @@ def gb_parse_xml_fetch_results(gb_xml):
         result_list.append(result)
     return result_list
 
-def ncbi_taxonomy(gb_fetch_results, batch_size=500, api_key = False):
+def ncbi_taxonomy(gb_fetch_results, batch_size=500, api_key = None):
     """
     Orchestrates making calls to the NCBI efetch service, querying the NCBI
     Taxonomy database for a list of NCBI taxids. Passes off the XML to the
@@ -228,6 +232,10 @@ def ncbi_taxonomy(gb_fetch_results, batch_size=500, api_key = False):
     :param int batch_size:
         The number of results to request at a time -- the higher the better, but 
         too large of a result set causes errors
+    :param str api_key:
+        A personal NCBI API key, obtained by creating an NCBI account. An API key 
+        is not required, but supplying one will increase API call rate to NCBI from 
+        three per second to 10 per second. 
     :rtype: list of dictionaries
     """
 
